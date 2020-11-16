a4626f1647bffd18baefbacaecbd68e5d4e9e2d2
NiklasLehnfeld <niklas@lehnfeld.net>
2020-11-16 09:49:22 +0100

    added script for building releases and renaming them correctly

---
+++ build_releases.sh
@@ -0,0 +1,17 @@
+mkdir release-builds
+
+artifact_name=$(cd android && ./gradlew printFullArtifactName | grep 'netzpolitik')
+
+## build android apk and move it to release-builds folder
+flutter build apk
+mv build/app/outputs/apk/release/*.apk release-builds
+
+## build ios Runners.app, convert it to ipa and move it to release-builds folder
+flutter build ios
+
+mkdir -p release-builds/Payload
+mv ./build/ios/iphoneos/Runner.app release-builds/Payload
+zip -r -y release-builds/Payload.zip release-builds/Payload/Runner.app
+mv release-builds/Payload.zip "release-builds/${artifact_name}.ipa"
+
+rm -Rf release-builds/Payload
