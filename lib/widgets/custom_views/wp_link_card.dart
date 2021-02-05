db7f1579353cd18fa3f08f07369958cef4057c5d
Niklas Lehnfeld <niklas@lehnfeld.net>
2021-02-05 21:26:34 +0100

    rework on imprint bottom sheet

---
+++ lib/widgets/custom_views/wp_link_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:wordpress_blog_app_template/extensions/context_ext.dart';
+import 'package:wordpress_blog_app_template/widgets/custom_views/width.dart';
+
+import 'wp_card.dart';
+
+class WPLinkCard extends StatelessWidget {
+
+  final IconData icon;
+  final String text;
+  final String link;
+  final Color backgroundColor;
+
+  const WPLinkCard(this.icon,
+      this.text,
+      this.link,
+      this.backgroundColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return WPCard(
+      onTap: () => launch(link),
+      child: Container(
+          padding: EdgeInsets.only(left: 10, top: 5, bottom: 5, right: 10),
+          width: context.isWide ? context.width / 2 - 30 : null,
+          color: backgroundColor,
+          child: Row(
+            children: [
+              FaIcon(
+                icon,
+                color: Colors.white,
+                size: 25,
+              ),
+              Width(20),
+              Text(
+                text,
+                style: context.headline5.copyWith(color: Colors.white),
+              ),
+              Spacer(),
+              FaIcon(
+                FontAwesomeIcons.arrowRight,
+                color: Colors.white,
+              ),
+            ],
+          )),
+    );
+
+  }
+
+}
--- lib/widgets/dashboard/imprint/imprint_bottom_sheet.dart
+++ lib/widgets/dashboard/imprint/imprint_bottom_sheet.dart
@@ -1,46 +1,70 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_html/rich_text_parser.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:wordpress_blog_app_template/widgets/custom_views/wp_bottom_sheet.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wordpress_blog_app_template/extensions/context_ext.dart';
+import 'package:wordpress_blog_app_template/widgets/custom_views/height.dart';
+import 'package:wordpress_blog_app_template/widgets/custom_views/wp_bottom_sheet.dart';
+import 'package:wordpress_blog_app_template/widgets/custom_views/wp_link_card.dart';
 
 class ImprintBottomSheet extends StatelessWidget {
-
   const ImprintBottomSheet();
 
   @override
   Widget build(BuildContext context) {
 
+    var cards = [
+      WPLinkCard(
+        FontAwesomeIcons.github,
+        context.getString('github_headline'),
+        context.getString('github_url'),
+        Colors.black,
+      ),
+      WPLinkCard(
+        FontAwesomeIcons.linkedin,
+        context.getString('linkedin_headline'),
+        context.getString('linkedin_url'),
+        Colors.blueAccent[700],
+      ),
+    ];
+
+    var bottomContainer = context.isWide ?
+      Row(children: cards) : Column(children: cards);
+
     return WPBottomSheet(
-        child: RichText(
-          text: TextSpan(
-            style: context.headline1,
-            children: <TextSpan>[
-              TextSpan(text: context.getString('about_the_app_headline') + '\n'),
-              TextSpan(text: context.getString('about_the_app_content') , style: context.body1),
-
-              LinkTextSpan(
-                  text: context.getString('github_link') + '\n\n',
-                  url: context.getString('github_link'),
-                  onLinkTap: (url) => launch(url),
-                  style: context.body1.copyWith(color: context.primaryColor, decoration: TextDecoration.underline,),
-              ),
-
-              TextSpan(text: context.getString('about_the_dev_headline') + '\n'),
-              TextSpan(text: context.getString('about_the_dev_content') + '\n', style: context.body1),
-              LinkTextSpan(
-                text: context.getString('linkedin_url') + '\n\n',
-                url: context.getString('linkedin_url'),
-                onLinkTap: (url) => launch(url),
-                style: context.body1.copyWith(color: context.primaryColor, decoration: TextDecoration.underline,),
-              ),
-
-              TextSpan(text: context.getString('imprint_headline') + '\n'),
-              TextSpan(text: context.getString('imprint_content') + '\n\n', style: context.body1),
-            ],
-          ),
-        )
-    );
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildAboutAppSection(context),
+        Height(15),
+        _buildImprintSection(context),
+        Height(15),
+        bottomContainer,
+        Height(20),
+      ],
+    ));
   }
-}
 
+  Widget _buildAboutAppSection(BuildContext context) => RichText(
+        text: TextSpan(
+          style: context.headline1,
+          children: <TextSpan>[
+            TextSpan(text: context.getString('about_the_app_headline') + '\n'),
+            TextSpan(
+                text: context.getString('about_the_app_content'),
+                style: context.body2),
+          ],
+        ),
+      );
+
+  Widget _buildImprintSection(BuildContext context) => RichText(
+        text: TextSpan(
+          style: context.headline1,
+          children: <TextSpan>[
+            TextSpan(text: context.getString('imprint_headline') + '\n'),
+            TextSpan(
+                text: context.getString('imprint_content'),
+                style: context.body2),
+          ],
+        ),
+      );
+}
