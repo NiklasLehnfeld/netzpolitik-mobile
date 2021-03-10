f7647e1dce436b0a941885a6383aebd00b1baf57
Niklas Lehnfeld <niklas@lehnfeld.net>
2021-03-10 23:21:43 +0100

    removed every dependency to push notifications

--- lib/providers.dart
+++ lib/providers.dart
@@ -4,7 +4,6 @@ import 'package:netzpolitik_mobile/config/rest_configuration.dart';
 import 'package:netzpolitik_mobile/logic/audio_player.dart';
 import 'package:netzpolitik_mobile/persistence/article_dao.dart';
 import 'package:netzpolitik_mobile/persistence/wp_database.dart';
-import 'package:netzpolitik_mobile/rest/push_notifications.dart';
 import 'package:netzpolitik_mobile/rest/rest_client.dart';
 import 'package:provider/provider.dart';
 
@@ -23,9 +22,6 @@ class Providers {
 
     //logic
     ChangeNotifierProvider<AudioPlayer>(create: (context) => AudioPlayer()),
-
-    //push-notifications
-    Provider<PushNotificationsManager>(create: (context) => PushNotificationsManager()),
 
     //persistence
     Provider<WPDatabase>(create: (context) => WPDatabase()),
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,19 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:netzpolitik_mobile/extensions/context_ext.dart';
-import 'package:netzpolitik_mobile/feature_management/features.dart';
-import 'package:netzpolitik_mobile/rest/push_notifications.dart';
 import 'package:netzpolitik_mobile/widgets/custom_views/wp_appbar.dart';
 import 'package:netzpolitik_mobile/widgets/custom_views/wp_bottom_navigation.dart';
 import 'package:netzpolitik_mobile/widgets/dashboard/articles/articles_widget.dart';
 import 'package:netzpolitik_mobile/widgets/dashboard/bookmarks/bookmarks_widget.dart';
 import 'package:netzpolitik_mobile/widgets/dashboard/categories/categories_widget.dart';
-import 'package:provider/provider.dart';
-
-import 'dialogs/notification_permission_dialog.dart';
 
 
 class Home extends StatefulWidget {
@@ -28,9 +21,6 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-
-    initPushNotifications(context);
-
     return Scaffold(
       appBar: WPAppBar(),
       bottomNavigationBar: WPBottomNavigation(
@@ -46,28 +36,6 @@ class _HomeState extends State<Home> {
         index: _currentIndex,
       )
     );
-  }
-
-  void initPushNotifications(BuildContext context) async {
-
-    if (!Feature.PUSH_NOTIFICATIONS.isEnabled()) {
-      return;
-    }
-
-    var pushNotificationManager = context.watch<PushNotificationsManager>();
-
-    await Future.delayed(Duration(seconds: 3));
-
-    if (Platform.isIOS && await pushNotificationManager.shouldAskForPermission()) {
-      await showDialog(
-        context: context,
-        builder: (context) => NotificationPermissionDialog(),
-        barrierColor: context.primaryColor.withAlpha(125),
-      );
-    }
-
-    pushNotificationManager.initialize();
-
   }
 }
 
