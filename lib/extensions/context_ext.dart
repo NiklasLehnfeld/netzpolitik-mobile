7c698c47e5db59e22c03c66785becb6a38fbee90
NiklasLehnfeld <niklas.lehnfeld@volkswagen.de>
2024-11-20 21:50:16 +0100

    fixes grid view

--- lib/extensions/context_ext.dart
+++ lib/extensions/context_ext.dart
@@ -30,20 +30,20 @@ extension ContextExteinsions on BuildContext {
   bool get isDarkTheme => Theme.of(this).brightness == Brightness.dark;
 
 
-  TextStyle get headline1 => Theme.of(this).textTheme.headlineLarge!;
-  TextStyle get headline2 => Theme.of(this).textTheme.headlineMedium!;
-  TextStyle get headline3 => Theme.of(this).textTheme.headlineSmall!;
-  TextStyle get headline4 => Theme.of(this).textTheme.titleLarge!;
-  TextStyle get headline5 => Theme.of(this).textTheme.titleMedium!;
-  TextStyle get headline6 => Theme.of(this).textTheme.titleSmall!;
+  TextStyle get headline1 => Theme.of(this).textTheme.displayLarge!;
+  TextStyle get headline2 => Theme.of(this).textTheme.displayMedium!;
+  TextStyle get headline3 => Theme.of(this).textTheme.displaySmall!;
+  TextStyle get headline4 => Theme.of(this).textTheme.headlineLarge!;
+  TextStyle get headline5 => Theme.of(this).textTheme.headlineMedium!;
+  TextStyle get headline6 => Theme.of(this).textTheme.headlineSmall!;
 
-  TextStyle get body1 => Theme.of(this).textTheme.bodyLarge!;
-  TextStyle get body2 => Theme.of(this).textTheme.bodyMedium!;
-  TextStyle get quote => Theme.of(this).textTheme.bodySmall!.copyWith(
+  TextStyle get body1 => Theme.of(this).textTheme.bodyMedium!;
+  TextStyle get body2 => Theme.of(this).textTheme.bodySmall!;
+  TextStyle get quote => Theme.of(this).textTheme.labelSmall!.copyWith(
     fontStyle: FontStyle.italic
   );
 
-  TextStyle get caption => Theme.of(this).textTheme.displaySmall!;
+  TextStyle get caption => Theme.of(this).textTheme.labelSmall!;
 
   Color get primaryColor => Theme.of(this).primaryColor;
   Color get scaffoldColor => Theme.of(this).scaffoldBackgroundColor;
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -3,39 +3,31 @@ import 'package:netzpolitik_mobile/extensions/context_ext.dart';
 
 var baseTextStyle = TextStyle();
 
-ThemeData lightTheme(BuildContext context, double fontSize) => ThemeData(
-    primarySwatch: Colors.blue,
-    hintColor: Colors.grey,
-    scaffoldBackgroundColor: Colors.grey[100],
-    textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.blue),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: Colors.white,
-    ),
-    textTheme: TextTheme(
-      headlineLarge: baseTextStyle.copyWith(
-          fontSize: fontSize + 6,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      headlineMedium: baseTextStyle.copyWith(
-          fontSize: fontSize + 5,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      headlineSmall: baseTextStyle.copyWith(
-          fontSize: fontSize + 4,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      titleLarge: baseTextStyle.copyWith(
-          fontSize: fontSize + 3,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      titleMedium: baseTextStyle.copyWith(
-          fontSize: fontSize + 2,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      titleSmall: baseTextStyle.copyWith(
-          fontSize: fontSize + 1,
-          color: context.primaryColor,
-          fontWeight: FontWeight.bold),
-      bodyLarge: baseTextStyle.copyWith(fontSize: fontSize),
-      bodyMedium: baseTextStyle.copyWith(fontSize: fontSize - 3),
-    ));
+ThemeData lightTheme(BuildContext context, double fontSize) =>
+    ThemeData(
+      primarySwatch: Colors.blue,
+      hintColor: Colors.grey,
+      scaffoldBackgroundColor: Colors.grey[100],
+      textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.blue),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+      ),
+      textTheme: TextTheme(
+        displayLarge: baseTextStyle.copyWith(
+            fontSize: fontSize + 6, fontWeight: FontWeight.bold),
+        displayMedium: baseTextStyle.copyWith(
+            fontSize: fontSize + 5, fontWeight: FontWeight.bold),
+        displaySmall: baseTextStyle.copyWith(
+            fontSize: fontSize + 4, fontWeight: FontWeight.bold),
+        headlineLarge: baseTextStyle.copyWith(
+            fontSize: fontSize + 3, fontWeight: FontWeight.bold),
+        headlineMedium: baseTextStyle.copyWith(
+            fontSize: fontSize + 2, fontWeight: FontWeight.bold),
+        headlineSmall: baseTextStyle.copyWith(
+            fontSize: fontSize + 1, fontWeight: FontWeight.bold),
+        bodyLarge: baseTextStyle.copyWith(fontSize: fontSize),
+        bodyMedium: baseTextStyle.copyWith(fontSize: fontSize - 2),
+        bodySmall: baseTextStyle.copyWith(fontSize: fontSize - 4),
+      ),
+    );
+
