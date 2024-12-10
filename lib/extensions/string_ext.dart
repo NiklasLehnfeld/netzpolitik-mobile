01899b11164ce1aab7546eeacd2819c18a993d19
NiklasLehnfeld <niklas.lehnfeld@volkswagen.de>
2024-12-10 21:30:12 +0100

    fixes stackoverflow when replacing audio content

--- lib/extensions/string_ext.dart
+++ lib/extensions/string_ext.dart
@@ -4,8 +4,10 @@ extension MP3Checker on String {
 
   bool get containsMP3 => RegExp(MP3_REGEX).hasMatch(this);
 
-  String get withoutMp3 => replaceAll(RegExp('<p>(.|\n)*<\/audio><\/p>\n'), '');
-
+  String get withoutMp3 {
+    final regex = RegExp(r'<p>.*?</audio></p>\n?', dotAll: true);
+    return replaceAll(regex, '');
+  }
   String? get mp3Url => RegExp(MP3_REGEX).firstMatch(this)?.group(1);
 
 }
